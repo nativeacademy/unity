87acb5a8e9c580d6fa93bc8f51416ab7a3c79905
aaa <b0844132@gmail.com>
2013-12-22 15:33:11 +0900

    アニメあたり

---
+++ Assets/Scripts/AnimController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimController : MonoBehaviour {
+
+	private Animator _anim;
+
+	// Use this for initialization
+	void Start () {
+		_anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartRun() {
+		_anim.SetBool ("Run", true);
+	}
+
+	public void EndRun() {
+		_anim.SetBool ("Run", false);
+	}
+
+	public void StartThrow() {
+		_anim.SetBool ("Shoot", true);
+	}
+
+	public void EndThrow() {
+		_anim.SetBool ("Shoot", false);
+	}
+}
